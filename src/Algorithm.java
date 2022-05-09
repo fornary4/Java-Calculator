b93f1ecd05e7affcd7b8c46a154444667b1698e5
fornary4 <2839395512@qq.com>
2022-05-09 19:20:16 +0800

    update

--- src/Algorithm.java
+++ src/Algorithm.java
@@ -2,89 +2,96 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Stack;
 import java.util.StringTokenizer;
-public class Algorithm
-{
-    private final ArrayList<String> ListPreExpression= new ArrayList<>(); //存放原始表达式
-    private final ArrayList<String> ListExpression= new ArrayList<>();   //存放后缀表达式
-    private final Stack<String> StackOperation=new Stack<>();   //运算数栈
-    private final Stack<String> StackOperator=new Stack<>();  //运算符栈
+
+public class Algorithm {
+    private final ArrayList<String> ListPreExpression = new ArrayList<>(); //存放原始表达式
+    private final ArrayList<String> ListExpression = new ArrayList<>();   //存放后缀表达式
+    private final Stack<String> StackOperation = new Stack<>();   //运算数栈
+    private final Stack<String> StackOperator = new Stack<>();  //运算符栈
+
     //分割表达式，构造方法
-    public Algorithm(String PreExpression)
-    {
+    public Algorithm(String PreExpression) {
         //以+-*/%^（）为分割符，对原始表达式进行分割，并返回分割符
-        StringTokenizer stringTokenizer=new StringTokenizer(PreExpression, "^%+-*/()",true);
+        StringTokenizer stringTokenizer = new StringTokenizer(PreExpression, "^%+-*/()", true);
         //正则表达式："\\^|\\%|\\+|\\-|\\*|\\/|(|\\)"
-        while(stringTokenizer.hasMoreTokens())
-        {
+        while (stringTokenizer.hasMoreTokens()) {
             ListPreExpression.add(stringTokenizer.nextToken());
         }
-        for (int i=0;i<ListPreExpression.size();i++){
-            if(ListPreExpression.get(0).equals("-"))
-                ListPreExpression.add(0,"0");          //处理表达式初始负号
-            if(ListPreExpression.get(i).equals("Π")){               //替换Π
-                String tmp=Double.toString(Math.PI);
-                ListPreExpression.set(i,tmp);
+        for (int i = 0; i < ListPreExpression.size(); i++) {
+            if (ListPreExpression.get(0).equals("-"))
+                ListPreExpression.add(0, "0");          //处理表达式初始负号
+            if (ListPreExpression.get(i).equals("Π")) {               //替换Π
+                String tmp = Double.toString(Math.PI);
+                ListPreExpression.set(i, tmp);
             }
-            if (ListPreExpression.get(i).equals("-")&&ListPreExpression.get(i-1).equals("("))
-                ListPreExpression.add(i,"0");               //处理负数
+            if (ListPreExpression.get(i).equals("-") && ListPreExpression.get(i - 1).equals("("))
+                ListPreExpression.add(i, "0");               //处理负数
         }
     }
 
 
     //判断是否为运算符
-    public boolean isOperator(String str)
-    {
+    public boolean isOperator(String str) {
         return "+-*/%^tancossinlgln()".contains(str);
     }
 
     //获取运算符的优先级
-    public int getPriority(String str)
-    {
+    public int getPriority(String str) {
         int a;
-        switch(str)
-        {
+        switch (str) {
             case "+":
-            case "-":a=1;break;
+            case "-":
+                a = 1;
+                break;
             case "*":
             case "/":
-            case "%":a=2;break;
-            case "(":a=5;break;
-            case ")":a=0;break;
+            case "%":
+                a = 2;
+                break;
+            case "(":
+                a = 5;
+                break;
+            case ")":
+                a = 0;
+                break;
             case "tan":
             case "sin":
             case "cos":
             case "ln":
-            case "lg":a=4;break;
-            case "^":a=3;break;
-            default: a=-1;break;
+            case "lg":
+                a = 4;
+                break;
+            case "^":
+                a = 3;
+                break;
+            default:
+                a = -1;
+                break;
         }
         return a;
     }
 
     //比较优先级大小
-    public boolean ComparePriority(String str1,String str2)
-    {
-        return getPriority(str1)>getPriority(str2);
+    public boolean ComparePriority(String str1, String str2) {
+        return getPriority(str1) > getPriority(str2);
     }
 
     //定义运算符入栈规则
-    public void OperatorToStack(String str)
-    {
-        if(StackOperator.isEmpty())           //若栈为空，直接压栈
+    public void OperatorToStack(String str) {
+        if (StackOperator.isEmpty())           //若栈为空，直接压栈
             StackOperator.push(str);
-        else if("(".equals(str))              //若运算符为"("，压栈
+        else if ("(".equals(str))              //若运算符为"("，压栈
             StackOperator.push(str);
-        else if(")".equals(str))              //若运算符为")"，将"("和")"之间的操作符存放到ListExpression中
+        else if (")".equals(str))              //若运算符为")"，将"("和")"之间的操作符存放到ListExpression中
         {
             String tmp;
-            while(!"(".equals(tmp=StackOperator.pop()))
+            while (!"(".equals(tmp = StackOperator.pop()))
                 ListExpression.add(tmp);
-        }
-        else if("(".equals(StackOperator.peek()))  //若栈顶元素为"("，压栈
+        } else if ("(".equals(StackOperator.peek()))  //若栈顶元素为"("，压栈
             StackOperator.push(str);
-        else if(ComparePriority(str,StackOperator.peek()))//若str优先级大于栈顶元素则压栈
+        else if (ComparePriority(str, StackOperator.peek()))//若str优先级大于栈顶元素则压栈
             StackOperator.push(str);
-        else if(!ComparePriority(str,StackOperator.peek()))//若小于则将栈顶元素弹出，递归比较下一个元素
+        else if (!ComparePriority(str, StackOperator.peek()))//若小于则将栈顶元素弹出，递归比较下一个元素
         {
             ListExpression.add(StackOperator.pop());
             OperatorToStack(str);
@@ -93,19 +100,17 @@ public class Algorithm
 
 
     //中缀表达式转换成后缀表达式
-    public void ListPreExpressionToListExpression()
-    {
-        for(String str:ListPreExpression)         //遍历ListPreExpression，运算符加入OperatorToStack,数字加入ListExpression
+    public void ListPreExpressionToListExpression() {
+        for (String str : ListPreExpression)         //遍历ListPreExpression，运算符加入OperatorToStack,数字加入ListExpression
         {
-            if(isOperator(str))
+            if (isOperator(str))
                 OperatorToStack(str);
             else
                 ListExpression.add(str);
         }
 
         //遍历完原始表达式后，将StackOperator栈内的运算符全部添加到ListExpression中
-        while(!StackOperator.isEmpty())
-        {
+        while (!StackOperator.isEmpty()) {
             ListExpression.add(StackOperator.pop());
         }
     }
@@ -114,12 +119,12 @@ public class Algorithm
     //定义计算方法
 
     //双目运算
-    public int flag =0;//判断输入格式是否正确
-    public double Operation2(String str1,String str2,String str3)
-    {
-        double num1,num2;
-        num2= Double.parseDouble(str1);
-        num1= Double.parseDouble(str2);
+    public int flag = 0;//判断输入格式是否正确
+
+    public double Operation2(String str1, String str2, String str3) {
+        double num1, num2;
+        num2 = Double.parseDouble(str1);
+        num1 = Double.parseDouble(str2);
         switch (str3) {
             case "+":
                 return num1 + num2;
@@ -139,10 +144,9 @@ public class Algorithm
     }
 
     //单目运算
-    public double Operation1(String str1,String str2)
-    {
+    public double Operation1(String str1, String str2) {
         double num1;
-        num1= Double.parseDouble(str1);
+        num1 = Double.parseDouble(str1);
         switch (str2) {
             case "tan":
                 if (num1 == Math.PI / 2)           //tan（π/2）不存在，flag=2
@@ -160,53 +164,45 @@ public class Algorithm
     }
 
     //计算后缀表达式
-    public String Calculate_ListExpression()
-    {
-        for(String str:ListExpression)
-        {
-            if(str.equals("(")||str.equals(")"))      //若ListExpression中含有括号，则报错
-                flag =3;
-            else if(!isOperator(str))          //把数字存放进运算栈中
+    public String Calculate_ListExpression() {
+        for (String str : ListExpression) {
+            if (str.equals("(") || str.equals(")"))      //若ListExpression中含有括号，则报错
+                flag = 3;
+            else if (!isOperator(str))          //把数字存放进运算栈中
                 StackOperation.push(str);
-            else if("tansincoslgln".contains(str))    //tan、sin、cos、lg、ln进行单目运算
-                StackOperation.push(String.valueOf(Operation1(StackOperation.pop(),str)));
+            else if ("tansincoslgln".contains(str))    //tan、sin、cos、lg、ln进行单目运算
+                StackOperation.push(String.valueOf(Operation1(StackOperation.pop(), str)));
             else
                 //其它运算符进行双目运算
-                StackOperation.push(String.valueOf(Operation2(StackOperation.pop(),StackOperation.pop(),str)));
+                StackOperation.push(String.valueOf(Operation2(StackOperation.pop(), StackOperation.pop(), str)));
         }
-        if(flag ==1|| flag ==3)
-        {
-            flag =0;
+        if (flag == 1 || flag == 3) {
+            flag = 0;
             return "ILLEGAL!";
-        }
-        else if(flag ==2)
-        {
-            flag =0;
+        } else if (flag == 2) {
+            flag = 0;
             return "NON-EXISTENT!";
-        }
-        else
+        } else
             return StackOperation.pop();
     }
 
-//命令行输入测试
-    public static void main(String[] args)
-    {
-        Scanner scanner=new Scanner(System.in);
-        while (scanner.hasNextLine()){
-            Algorithm algorithm=new Algorithm(scanner.nextLine());
+    //命令行输入测试
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            Algorithm algorithm = new Algorithm(scanner.nextLine());
             algorithm.ListPreExpressionToListExpression();
-            if(algorithm.Calculate_ListExpression().equals("ILLEGAL!"))
+            if (algorithm.Calculate_ListExpression().equals("ILLEGAL!"))
                 System.out.println("ILLEGAL!");
-            else if(algorithm.Calculate_ListExpression().equals("NON-EXISTENT!"))
+            else if (algorithm.Calculate_ListExpression().equals("NON-EXISTENT!"))
                 System.out.println("NON-EXISTENT!");
-            else
-            {
-                double result= Double.parseDouble(algorithm.Calculate_ListExpression());
+            else {
+                double result = Double.parseDouble(algorithm.Calculate_ListExpression());
                 //数据类型转换
-                long value =(long)result;
-                double d_value =result- value;
+                long value = (long) result;
+                double d_value = result - value;
                 //去掉无意义的0
-                if(d_value ==0)
+                if (d_value == 0)
                     System.out.println(value);
                 else
                     System.out.println(result);
--- src/GUICalculator.java
+++ src/GUICalculator.java
@@ -3,44 +3,41 @@ import java.awt.event.ActionListener;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class GUICalculator extends JFrame
-{
-    JFrame frame=new JFrame("科学计算器");
-    JTextField display=new JTextField("0");
-    Font font_display =new Font("Consolas",Font.BOLD,42);       //设置字体样式
-    Font font_button =new Font("Consolas",Font.BOLD,21);
-    Image iconImage=(new ImageIcon("img/图标.jpeg")).getImage();//设置图标
-    actionlistener al=new actionlistener();//创建监听器action listener的对象
+public class GUICalculator extends JFrame {
+    JFrame frame = new JFrame("科学计算器");
+    JTextField display = new JTextField("0");
+    Font font_display = new Font("Consolas", Font.BOLD, 42);       //设置字体样式
+    Font font_button = new Font("Consolas", Font.BOLD, 21);
+    Image iconImage = (new ImageIcon("img/图标.jpeg")).getImage();//设置图标
+    actionlistener al = new actionlistener();//创建监听器action listener的对象
 
     //设置计算器按键
-    JButton button_clear=new JButton("C");
-    JButton button_delete=new JButton("←");
-    JButton button_plus=new JButton("+");
-    JButton button_minus=new JButton("-");
-    JButton button_multiply=new JButton("*");
-    JButton button_divide=new JButton("/");
-    JButton button_spot=new JButton(".");
-    JButton button_equal =new JButton("=");
-    JButton button_sin=new JButton("sin");
-    JButton button_cos=new JButton("cos");
-    JButton button_tan=new JButton("tan");
-    JButton button_ln=new JButton("ln");
-    JButton button_bracket_r=new JButton("(");
-    JButton button_bracket_l=new JButton(")");
-    JButton button_mod=new JButton("%");
-    JButton button_lg=new JButton("lg");
-    JButton button_pow=new JButton("^");
-    JButton button_pi=new JButton("Π");
-    JButton[] button=new JButton[10];
+    JButton button_clear = new JButton("C");
+    JButton button_delete = new JButton("←");
+    JButton button_plus = new JButton("+");
+    JButton button_minus = new JButton("-");
+    JButton button_multiply = new JButton("*");
+    JButton button_divide = new JButton("/");
+    JButton button_spot = new JButton(".");
+    JButton button_equal = new JButton("=");
+    JButton button_sin = new JButton("sin");
+    JButton button_cos = new JButton("cos");
+    JButton button_tan = new JButton("tan");
+    JButton button_ln = new JButton("ln");
+    JButton button_bracket_r = new JButton("(");
+    JButton button_bracket_l = new JButton(")");
+    JButton button_mod = new JButton("%");
+    JButton button_lg = new JButton("lg");
+    JButton button_pow = new JButton("^");
+    JButton button_pi = new JButton("Π");
+    JButton[] button = new JButton[10];
 
-    public GUICalculator()
-    {
+    public GUICalculator() {
         super();
 
         //设置按钮颜色字体和监听器
-        for (int i = 0; i < 10; i++)
-        {
-            button[i] = new JButton(""+i);
+        for (int i = 0; i < 10; i++) {
+            button[i] = new JButton("" + i);
             button[i].setBackground(Color.white);
             button[i].setFont(font_button);
             button[i].addActionListener(al);
@@ -112,15 +109,15 @@ public class GUICalculator extends JFrame
         display.setBackground(Color.white);
         display.setEditable(false);          //设置文本框可编辑
 
-        JPanel pan1=new JPanel();                       //设置文本框布局
-        pan1.setLayout(new GridLayout(1,1,5,5));
+        JPanel pan1 = new JPanel();                       //设置文本框布局
+        pan1.setLayout(new GridLayout(1, 1, 5, 5));
         pan1.setBorder(BorderFactory.createEmptyBorder(5, 5, 0, 5));
         pan1.add(display);
-        NewPanel panBody=new NewPanel();                    //将按键加入panBody中
+        NewPanel panBody = new NewPanel();                    //将按键加入panBody中
 
         frame.getContentPane().add(panBody);//将含画板的面板加入
 
-        panBody.setLayout(new GridLayout(7,4));
+        panBody.setLayout(new GridLayout(7, 4));
         panBody.add(button_clear);
         panBody.add(button_delete);
         panBody.add(button_bracket_r);
@@ -154,13 +151,13 @@ public class GUICalculator extends JFrame
 
         //创建框架窗口
         frame.setLayout(new BorderLayout());       //布局
-        frame.add(pan1,BorderLayout.NORTH);
-        frame.add(panBody,BorderLayout.CENTER);
+        frame.add(pan1, BorderLayout.NORTH);
+        frame.add(panBody, BorderLayout.CENTER);
         frame.setSize(540, 440);                   //大小
         frame.setLocation(460, 150);               //位置
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
+
         //注册监听器
         button_plus.addActionListener(al);
         button_minus.addActionListener(al);
@@ -209,53 +206,45 @@ public class GUICalculator extends JFrame
 
 
     //定义处理事件的类
-    public class actionlistener implements ActionListener
-    {
+    public class actionlistener implements ActionListener {
         Algorithm algorithm;
-        public String pre_display="";
-        public void actionPerformed(ActionEvent e)
-        {
-            Object click1=e.getSource();             //获得组件名称
-            String click2=e.getActionCommand();      //获得组件标签
-            if(click1==button_clear)                 //初始化文本框
+        public String pre_display = "";
+
+        public void actionPerformed(ActionEvent e) {
+            Object click1 = e.getSource();             //获得组件名称
+            String click2 = e.getActionCommand();      //获得组件标签
+            if (click1 == button_clear)                 //初始化文本框
             {
-                pre_display="";
+                pre_display = "";
                 display.setText("0");
-            }
-            else if(click1==button_delete)           //用handleClear()方法处理delete
+            } else if (click1 == button_delete)           //用handleClear()方法处理delete
             {
-                if(!pre_display.equals(""))
+                if (!pre_display.equals(""))
                     display.setText(handleDelete(pre_display));
                 else
                     display.setText(handleDelete(display.getText()));
-            }
-            else if(click2.equals("="))
-            {
+            } else if (click2.equals("=")) {
                 //计算表达式
-                algorithm =new Algorithm(display.getText());
+                algorithm = new Algorithm(display.getText());
                 algorithm.ListPreExpressionToListExpression();
-                if(algorithm.Calculate_ListExpression().equals("ILLEGAL!"))
+                if (algorithm.Calculate_ListExpression().equals("ILLEGAL!"))
                     display.setText("ILLEGAL!");
-                else if(algorithm.Calculate_ListExpression().equals("NON-EXISTENT!"))
+                else if (algorithm.Calculate_ListExpression().equals("NON-EXISTENT!"))
                     display.setText("NON-EXISTENT!");
-                else
-                {
-                    double result= Double.parseDouble(algorithm.Calculate_ListExpression());
+                else {
+                    double result = Double.parseDouble(algorithm.Calculate_ListExpression());
                     //数据类型转换
-                    long value=(long)result;
-                    double d_value =result-value;
+                    long value = (long) result;
+                    double d_value = result - value;
                     //去掉无意义的0
-                    if(d_value ==0)
+                    if (d_value == 0)
                         display.setText(String.valueOf(value));
                     else
                         display.setText(String.valueOf(result));
                 }
 
-                pre_display="";
-            }
-
-            else
-            {
+                pre_display = "";
+            } else {
                 switch (click2) {
                     case "tan":
                         click2 = "tan(";
@@ -273,39 +262,39 @@ public class GUICalculator extends JFrame
                         click2 = "lg(";
                         break;
                 }
-                pre_display+=click2;
+                pre_display += click2;
                 display.setText(pre_display);
             }
         }
 
 
         //删除操作
-        public String handleDelete(String text)
-        {
-            int textLength=text.length();
-            if(textLength>0)
-            {
-                text=text.substring(0, textLength-1);     //去掉最后一个字符
-                if(text.length()==0)                     //若文本框为空，则初始化为“0”
+        public String handleDelete(String text) {
+            int textLength = text.length();
+            if (textLength > 0) {
+                text = text.substring(0, textLength - 1);     //去掉最后一个字符
+                if (text.length() == 0)                     //若文本框为空，则初始化为“0”
                 {
-                    text="";
+                    text = "";
                 }
                 pre_display = text;
                 return text;
-            }
-            else
+            } else
                 return "";
         }
 
     }
-    public static void main(String[] args)
-    {
-        GUICalculator mycalculator=new GUICalculator();
+
+    public static void main(String[] args) {
+        GUICalculator mycalculator = new GUICalculator();
         mycalculator.setResizable(true);
     }
+
     static class NewPanel extends JPanel {
         public NewPanel() {
         }
+
+        @Override
         public void paintComponent(Graphics g) {
             int x = 0, y = 0;
             ImageIcon icon = new ImageIcon("img/背景2.jpeg");
